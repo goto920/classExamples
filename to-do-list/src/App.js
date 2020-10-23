0394ad4bf23c8c87557460357be5baabaf183ca4
goto920 <goto@kmgoto.jp>
2020-10-23 12:43:08 +0900

    update

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,18 +6,35 @@ class App extends React.Component {
   constructor(props){
     super(); 
 
+    const toDoListSaved 
+      = JSON.parse(localStorage.getItem('toDoList')) || [];
+
     this.state = {
-      toDoList : [],
+      toDoList : toDoListSaved,
       title: "",
       description: "" 
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleAddItem = this.handleAddItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
 
   render () { // 描画処理
+
+    let toDoListDisplay = this.state.toDoList;
+    // add filter, sort here 
+    const toDoListUI = toDoListDisplay.map((item,index,array) => 
+    (<div key={index}>
+     {index}: Title: {item.title}<br />
+     Description: {item.description}<br />
+     Creation: {new Date(item.creation).toLocaleString()}<br />
+     <button name="delete" value={item.creation} 
+         onClick={this.deleteItem} >Delete</button>
+    <hr />
+    </div>
+    )); 
 
     return (
      <div className="App"> <hr />
@@ -33,6 +50,7 @@ class App extends React.Component {
      <button name="add" onClick={this.handleAddItem}> 
        Add ToDo Item</button>
      <hr />
+     {toDoListUI}
      </div>
     );
   }
@@ -53,24 +71,30 @@ class App extends React.Component {
       alert('Title is empty'); return;
     }
 
-    let newItem = {
+    const newItem = {
       title: this.state.title,
       description: this.state.description,
       creation: Date.now()
     };
     this.setState({title: "",description: ""});
 
-    let newList = [...this.state.toDoList, newItem];
+    const newList = [...this.state.toDoList, newItem];
     this.setState({toDoList: newList});
+    // console.log(JSON.stringify(this.state.toDoList));
+    // console.log(JSON.stringify(newList));
+    localStorage.setItem('toDoList', JSON.stringify(newList));
 
-    let message = 
-       newItem.title +"\n" + 
-       newItem.description +"\n" + newItem.creation;
-    alert(message);
+  }
 
-    // console.log(this.state.toDoList);
-    console.log(newList);
+  deleteItem(e){
+    const creation = parseInt(e.target.value);
+    const newList = this.state.toDoList;
 
+    const index = newList.findIndex(item => item.creation === creation);
+
+    if (index >= 0) newList.splice(index,1);
+    this.setState({toDoList: newList});
+    localStorage.setItem('toDoList', JSON.stringify(newList));
   }
 
 } 
